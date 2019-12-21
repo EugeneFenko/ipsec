a2a3ecc6fe6a17af4854eef8792059c6b3af69eb
eugenefenko <gt60@ya.ru>
2019-12-21 17:35:02 +0200

    Add calculator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { StartpageComponent } from './components/startpage/startpage.component';
 import { CalculatorComponent } from './components/calculator/calculator.component';
 import { ConfiguratorComponent } from './components/configurator/configurator.component';
@@ -15,6 +17,7 @@ import { NotfoundComponent } from './components/notfound/notfound.component';
 
 import { MaterialModule } from './material-module';
 import { ParticlesModule } from 'angular-particle';
+import { from } from 'rxjs';
 
 @NgModule({
   declarations: [
@@ -30,6 +33,8 @@ import { ParticlesModule } from 'angular-particle';
   ],
   imports: [
     BrowserModule,
+    FormsModule, 
+    ReactiveFormsModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
--- src/app/components/calculator/calculator.component.ts
+++ src/app/components/calculator/calculator.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-calculator',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calculator.component.scss']
 })
 export class CalculatorComponent implements OnInit {
+  constructor(private _fb: FormBuilder) { }
 
-  constructor() { }
+  algoFormGroup: FormGroup;
+  speedFormGroup: FormGroup;
+  
+  speed: number;
+  selectedAlgorithm: number;
+  allBits: number;
+  timeResult: number;
 
   ngOnInit() {
+
+    this.allBits = Math.pow(2, this.selectedAlgorithm);
+    this.timeResult = this.allBits / this.speed;
+
+    this.algoFormGroup = this._fb.group({
+      algoCtrl: ['', Validators.required]
+    });
+    this.speedFormGroup = this._fb.group({
+      speedInput: ['', Validators.required]
+    });
   }
 
+  
 }
